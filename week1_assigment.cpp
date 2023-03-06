1dba01c45a4df21fec25cdc264c10e88e2bf96cf
christian.rua@gmail.com <christian.rua@gmail.com>
2023-03-06 18:24:54 -0500

    working in week1 assigment

--- week1_assigment.cpp
+++ week1_assigment.cpp
@@ -4,25 +4,19 @@ using namespace std;
 
 const int N = 40;
 
-void sum(int *p,int n, int d[]){
-    int i;
-    *p = 0;
-    for(i=0; i<n; ++i){
-        *p = *p + d[i];
-    }    
+inline void sum(int *p, vector<int> d){
+    *p = *p + d.back();    
 }
 
 int main(){
     int i;
     int accum = 0;
-    //int data[N];
     vector<int> data;
     //https://www.geeksforgeeks.org/vector-in-cpp-stl/
     for(i = 0; i < N; ++i){
         data.push_back(i);
-        //data[i] = i;
-        sum(&accum, N, data);
-        printf("sum is %d\n", accum);
+        sum(&accum, data);
+        cout << "sum is " << accum;
     }
     return 0;
 }
